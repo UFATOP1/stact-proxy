167c66ec8ceba5e9614ed5b8bb11896ac2dd00aa
ufatop1.net <89125031+UFATOP1@users.noreply.github.com>
2021-10-04 23:35:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ let utm_term = ['ufatop1','แทงบอล','ผลบอล', 'พนัน�
     var site = querystring.parse(url.parse(req.url).query).url;
      // and redirect the user to /proxy/url
      console.log(req.headers)
-     res.redirect(unblockerConfig.prefix + 'https://ufapro888s.co/?utm_source=google&utm_medium='+utm_medium.randomElement()+'&utm_campaign='+utm_campaign.randomElement()+'&utm_term='+utm_term.randomElement());
+     res.redirect(unblockerConfig.prefix + 'https://ufapro888s.info/?utm_source=google&utm_medium='+utm_medium.randomElement()+'&utm_campaign='+utm_campaign.randomElement()+'&utm_term='+utm_term.randomElement());
      
  });
  
